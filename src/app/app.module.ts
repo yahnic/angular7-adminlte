1bbf22fd85bd0a2dc86f374cf88d756f23337a89
unknown <nditahyahnic@gmail.com>
2019-02-08 18:11:27 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    DashboardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    window.dispatchEvent(new Event('resize'));
+    document.body.className = 'hold-transition skin-blue sidebar-mini';
+  }
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnDestroy(): void {
+    document.body.className = '';
+}
+
+}
